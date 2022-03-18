1dfc73ea33d7a997752af1216c6fe9dfc43c065c
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-18 14:05:58 +0200

    Add files via upload

---
+++ Relations_02/src/main/java/entities/Driver.java
@@ -0,0 +1,33 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity(name = "drivers")
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    public Driver() {}
+    public Driver(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
