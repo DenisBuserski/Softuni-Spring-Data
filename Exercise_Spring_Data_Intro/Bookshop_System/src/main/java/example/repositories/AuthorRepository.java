84d9bfc1b34041bdee9ad4e55472cbd691017eb3
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-02 22:05:14 +0300

    Update AuthorRepository.java

--- Exercise_Spring_Data_Intro/Bookshop_System/src/main/java/example/repositories/AuthorRepository.java
+++ Exercise_Spring_Data_Intro/Bookshop_System/src/main/java/example/repositories/AuthorRepository.java
@@ -3,7 +3,6 @@ package example.repositories;
 import example.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalDate;
 import java.util.List;
 
