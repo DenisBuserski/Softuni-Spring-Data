8b1f91a96b8ae2628d0820a73475834949dca72c
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-02 22:02:18 +0300

    Update ConsoleRunner.java

--- Exercise_Spring_Data_Intro/Bookshop_System/src/main/java/example/ConsoleRunner.java
+++ Exercise_Spring_Data_Intro/Bookshop_System/src/main/java/example/ConsoleRunner.java
@@ -8,7 +8,6 @@ import example.services.SeedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
