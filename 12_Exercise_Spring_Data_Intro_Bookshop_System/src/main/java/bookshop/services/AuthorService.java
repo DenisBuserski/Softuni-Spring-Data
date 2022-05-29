c8c92170f4c260def39d02e01a5700761dfe7aa0
Denis Buserski <denis.buserski@gmail.com>
2022-05-29 09:07:14 +0300

    Add files via upload

---
+++ 12_Exercise_Spring_Data_Intro_Bookshop_System/src/main/java/bookshop/services/AuthorService.java
@@ -0,0 +1,8 @@
+package bookshop.services;
+
+import bookshop.entities.Author;
+
+public interface AuthorService {
+
+    Author getRandomAuthor();
+}
---
+++ 12_Exercise_Spring_Data_Intro_Bookshop_System/src/main/java/bookshop/services/CategoryService.java
@@ -0,0 +1,10 @@
+package bookshop.services;
+
+import bookshop.entities.Category;
+
+import java.util.Set;
+
+public interface CategoryService {
+
+    Set<Category> getRandomCategories();
+}
