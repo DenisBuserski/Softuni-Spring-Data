75a60894818c78149477e4cfe3ae917b9ca30d49
Denis Buserski <denis.buserski@gmail.com>
2023-04-25 07:55:15 +0300

    Added Annotations to User.class

--- Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/entities/User.java
+++ Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/entities/User.java
@@ -1,19 +1,38 @@
 package user_system.entities;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity(name = "users")
 public class User {
+    @Id
+    @@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
     private String email;
+
+    @Column(name = "registered_on")
     private LocalDateTime registeredOn;
+
+    @Column(name = "last_time_logged_in")
     private LocalDateTime lastTimeLoggedIn;
     private int age;
+
+    @Column(name = "is_deleted")
     private boolean isDeleted;
+
+    @Column(name = "born_town")
     private Town bornTown;
+
+    @Column(name = "currently_living")
     private Town currentlyLiving;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+    
     private User friends;
 }
