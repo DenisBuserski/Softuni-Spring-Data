a2f0711b664b96540473a6b44a1d5bcdb27a4aa2
Denis Buserski <denis.buserski@gmail.com>
2023-05-23 16:09:56 +0300

    Update createUser method

--- Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/services/UserService.java
+++ Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/services/UserService.java
@@ -1,5 +1,9 @@
 package user_system.services;
 
+import user_system.entities.Town;
+
+import java.time.LocalDateTime;
+
 public interface UserService {
-    void createUser();
+    void createUser(String username, String password, String email, LocalDateTime registrationDateTime, LocalDateTime lastTimeLoggedIn, int age, Town bornTown, Town currentlyLivingTown, String firstName, String lastName);
 }
--- Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/services/UserServiceImpl.java
+++ Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/services/UserServiceImpl.java
@@ -2,8 +2,11 @@ package user_system.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import user_system.entities.Town;
 import user_system.entities.User;
 import user_system.repositories.UserRepository;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -12,7 +15,8 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
-    public void createUser() {
-
+    public void createUser(String username, String password, String email, LocalDateTime registrationDateTime, LocalDateTime lastTimeLoggedIn, int age, Town bornTown, Town currentlyLivingTown, String firstName, String lastName) {
+        User user = new User();
+        userRepository.save(user);
     }
 }
