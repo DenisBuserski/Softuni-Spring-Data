a3b883bf13a6b447adf7448d75f03b764647b17f
Denis Buserski <denis.buserski@gmail.com>
2023-05-23 16:26:02 +0300

    Create addCountries method

--- Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/ConsoleRunner.java
+++ Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/ConsoleRunner.java
@@ -3,23 +3,41 @@ package user_system;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import user_system.services.CountryService;
 import user_system.services.UserService;
-
-import java.time.LocalDateTime;
 
 @Component
 public class ConsoleRunner implements CommandLineRunner {
     private final UserService userService;
+    private final CountryService countryService;
 
     @Autowired
-    public ConsoleRunner(UserService userService) {
+    public ConsoleRunner(UserService userService, CountryService countryService) {
         this.userService = userService;
+        this.countryService = countryService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        addCountries();
+
+
 //        this.userService.createUser("test-1", "password-test-1", "test@mail.bg", LocalDateTime.now(), LocalDateTime.now(), 20, null, null, "TestFirst1", "TestLast1");
 //        System.out.println(this.userService.getUserFullName("TestFirst1", "TestLast1"));
+    }
+
+    private void addCountries() {
+        this.countryService.createCountry("Bulgaria");
+        this.countryService.createCountry("Russia");
+        this.countryService.createCountry("USA");
+        this.countryService.createCountry("England");
+        this.countryService.createCountry("Brazil");
+        this.countryService.createCountry("France");
+        this.countryService.createCountry("Japan");
+        this.countryService.createCountry("China");
+        this.countryService.createCountry("Germany");
+        this.countryService.createCountry("Canada");
     }
 }
