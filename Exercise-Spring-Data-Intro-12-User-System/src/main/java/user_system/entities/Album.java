ed0054a70b5f39a747ff61ad61cb1b5bbd21c657
Denis Buserski <denis.buserski@gmail.com>
2023-05-23 14:47:21 +0300

    Add annotations to Album class

--- Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/entities/Album.java
+++ Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/entities/Album.java
@@ -1,6 +1,14 @@
 package user_system.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "albums")
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String backgroundColor;
