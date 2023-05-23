ab392e8ff7d598de27efa9e8b72b28c2db2a14d9
Denis Buserski <denis.buserski@gmail.com>
2023-05-23 14:49:53 +0300

    Add annotations to Picture class

--- Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/entities/Picture.java
+++ Exercise-Spring-Data-Intro-12-User-System/src/main/java/user_system/entities/Picture.java
@@ -1,9 +1,17 @@
 package user_system.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "pictures")
 public class Picture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String caption;
     private String path;
-    
+
 }
