795682ab70cf265d248860f4a45a6b1f1a9bc297
Denis Buserski <denis.buserski@gmail.com>
2023-09-06 21:29:43 +0300

    Update

--- Exercise-Spring-Data-Advanced-Quering-14/src/main/resources/udp_get_books_count_by_author.sql
+++ Exercise-Spring-Data-Advanced-Quering-14/src/main/resources/udp_get_books_count_by_author.sql
@@ -3,7 +3,7 @@ USE `spring_data_intro`;
 DROP PROCEDURE IF EXISTS udp_get_books_count_by_author;
 
 DELIMITER $$
-CREATE PROCEDURE udp_get_books_count_by_author(IN first_name VARCHAR(255), IN last_name VARCHAR(255))
+CREATE PROCEDURE udp_get_books_count_by_author(first_name VARCHAR(255), last_name VARCHAR(255))
 BEGIN
 SELECT COUNT(b.id)
 FROM spring_data_intro.books b
