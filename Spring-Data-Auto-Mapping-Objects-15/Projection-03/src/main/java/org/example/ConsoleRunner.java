22d70b1c9b6d59461a12b29ddeece58972b1813b
Denis Buserski <denis.buserski@gmail.com>
2023-09-07 16:05:55 +0300

    Update

--- Spring-Data-Auto-Mapping-Objects-15/Projection-03/src/main/java/org/example/ConsoleRunner.java
+++ Spring-Data-Auto-Mapping-Objects-15/Projection-03/src/main/java/org/example/ConsoleRunner.java
@@ -1,13 +1,16 @@
 package org.example;
 
 import org.example.entities.Employee;
+import org.example.entities.dto.EmployeeDTO;
 import org.example.services.EmployeeService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Component
 public class ConsoleRunner implements CommandLineRunner {
@@ -24,6 +27,11 @@ public class ConsoleRunner implements CommandLineRunner {
         Employee employee = new Employee("FistNameEm", "LastNameEM", BigDecimal.ONE, LocalDate.now(), manager);
         Employee employee1 = new Employee("FistNameEm1", "LastNameEM1", BigDecimal.ONE, LocalDate.now(), manager);
 
-        this.employeeService.save(manager);
+        Optional<Employee> managerOption = this.employeeService.findOneById(1);
+        Employee managerRes = managerOption.get();
+
+        ModelMapper mapper = new ModelMapper();
+        EmployeeDTO employeeDTO = mapper.map(managerRes, EmployeeDTO.class);
+        System.out.println(employeeDTO.toString());
     }
 }
