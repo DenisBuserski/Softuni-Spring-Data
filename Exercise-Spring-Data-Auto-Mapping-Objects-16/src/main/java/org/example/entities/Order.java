a30d453c13e241c30c5944b02973d37baad09a30
DenisKP <denis.buserski@gmail.com>
2023-09-11 11:16:31 +0300

    Update

--- Exercise-Spring-Data-Auto-Mapping-Objects-16/src/main/java/org/example/entities/Order.java
+++ Exercise-Spring-Data-Auto-Mapping-Objects-16/src/main/java/org/example/entities/Order.java
@@ -1,6 +1,7 @@
 package org.example.entities;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity(name = "orders")
@@ -12,6 +13,38 @@ public class Order {
     @ManyToOne
     private User buyer;
 
+    @ManyToMany(fetch = FetchType.EAGER)
     private Set<Game> products;
 
+    public Order() {
+    }
+
+    public Order(User buyer, Set<Game> products) {
+        this.buyer = buyer;
+        this.products = products;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+
+    public Set<Game> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Game> products) {
+        this.products = products;
+    }
 }
