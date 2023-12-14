95eb8c330535fc752b978deb28665385f097dfbb
DenisKP <denis.buserski@gmail.com>
2023-12-14 12:50:56 +0200

    Update

--- Exercise-JSON-Processing-18/src/main/java/org/example/product_shop/entities/categories/Category.java
+++ Exercise-JSON-Processing-18/src/main/java/org/example/product_shop/entities/categories/Category.java
@@ -1,6 +1,7 @@
 package org.example.product_shop.entities.categories;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity(name = "categories")
 public class Category {
@@ -31,5 +32,18 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
